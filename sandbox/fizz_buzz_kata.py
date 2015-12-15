55f2aa580a7ccc660f226698846c74f4c3125c65
wolf_35C8E8C769 <wolf@cyber-dojo.org>
2015-12-15 12:04:57 +0000

    11

--- sandbox/fizz_buzz_kata.py
+++ sandbox/fizz_buzz_kata.py
@@ -2,15 +2,16 @@
 class Fizzbuzz:
 
     def say(self, number):
+        handlerFizz = HandlerFizz(handlerBuzz)
+        handlerFizzbuzz = HandlerFizzbuzz(handlerFizz)
+        result = handlerFizzbuzz.handle(number)
+
         if number % 15 == 0:
             return "FizzBuzz"
         if number % 3 == 0:
             return "Fizz"
         if number % 5 == 0:
             return "Buzz"
-
-        handlerFizzbuzz = HandlerFizzbuzz(handlerFizz)
-        result = handlerFizzbuzz.handle(number)
 
         return str(number)
 
